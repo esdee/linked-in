820a7eaec740c9790962452abcf7e00512317e70
Shashy Dass <ShashyD@gmail.com>
2012-02-24 07:07:04 -0500

    job history + education added

--- screen.clj
+++ screen.clj
@@ -11,47 +11,5 @@ lein repl
 
 (js/alert "Hello World")
 
-; core.js
-(in-ns 'linked-in.core)
-
-(def profile (atom nil))
-
-(defn start
-  []
-  (remote/request
-    100
-    "profiles/client/100"
-    :on-success #(->> % :body reader/read-string (reset! profile))
-    :on-error #(js/alert "Error")))
-
-(start)
-
-(:lastName @profile)
-
-(def content-node (xpath "//div[@class='content']"))
-
-(defn names
-  [profile]
-  )
-(defn profile-template
-  [profile]
-  [:div.content
-    [:div.names (str (:firstName profile) " "
-                     (:lastName profile))]
-   ])
-
-(swap-content! content-node
-               (html (profile-template @profile)))
 
 
-(load "linked_in/dev_server")
-
-(require '[linked-in.dev-server :as ds])
-
-(ds/profile-client nil nil)
-
-(load "linked_in/app/view_macros/profile")
-
-(require '[linked-in.app.view-macros.profile :as profile])
-
-(profile/template {:lastName "Dass" :firstName "Shashy"})
--- src/cljs/linked_in/core.cljs
+++ src/cljs/linked_in/core.cljs
@@ -10,20 +10,87 @@
 
 (def content-node (xpath "//div[@class='content']"))
 
+(defn block-section
+  [id]
+  {:id (name id) :class "section" :style "display:block"})
+
+(defn full-name
+  [profile]
+  (str (:firstName profile) " " (:lastName profile)))
+
+(defn overview
+  [profile]
+  (let [positions (-> profile :positions :values)
+        educations (-> profile :educations :values)
+        pos-details (fn [p] [:li (:title p) [:span.at " at "]
+                             (-> p :company :name)])]
+    [:dl#overview
+     [:dt.summary-current {:style "display:block"} "Current"]
+     [:dd.summary-current {:style "display:block"}
+      [:ul.current (pos-details (first positions))]]
+     [:dt.summary-past {:style "display:block"} "Past"]
+     [:dd.summary-past {:style "display:block"}
+     [:ul.past (map pos-details (take 3 (rest positions)))]]
+     [:dt.summary-education {:style "display:block"} "Education"]
+     [:dd.summary-education {:style "display:block"}
+      [:ul (map (fn [e] [:li (:schoolName e)]) educations)]]]))
+
+(defn profile-header
+  [profile]
+  [:div.profile-header
+   [:div#profile-picture.image {:style "display:block"}
+    [:img {:src (:pictureUrl profile)
+           :width 80 :height 80
+           :alt (full-name profile)}]]
+   [:h1
+    [:span#name {:class "n fn"}
+     [:span.given-name (:firstName profile)]
+     [:span.family-name (:lastName profile)]]]
+   [:p.title {:style "display:block"}
+    (:headline profile)]
+   [:dl#headline {:class "demographic-info adr"}
+    [:dt "Location"] [:dd.locality (get-in profile [:location :name])]
+    [:dt "Industry"] [:dd.industry (:industry profile)]]
+   [:h2.section-title (str (full-name profile) "'s Overview")]
+    (overview profile)])
+
+(defn profile-summary
+  [profile]
+  [:div (block-section :profile-summary)
+   "Profile Summary"])
+
+(defn profile-skills
+  [profile]
+  [:div (block-section :profile-skills)
+   "Profile Skills"])
+
+(defn profile-experience
+  [profile]
+  [:div (block-section :profile-experience)
+   "Profile Experience"])
+
+(defn template
+  [profile]
+  [:div.content
+   (profile-header profile)
+   (profile-summary profile)
+   (profile-skills profile)
+   (profile-experience profile)])
+
 (defn show-profile
   [profile]
   (->> profile
-    profile/template
+    template
     html
     (swap-content! content-node)))
 
-(defn ^:export start
+(defn start
   []
   (remote/request
     100
     "profiles/client/100"
     :on-success #(->> % :body reader/read-string show-profile)
-    :on-error #(js/alert "Error")))
+    :on-error #(->> % :body (str "*ERROR* ") js/alert)))
 
 (defn ^:export repl
   []
